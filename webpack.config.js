57df94073b638f9fc3ce4bc60a81b114a7b312da
Kampanart.k <kampanart.k@cdg.co.th>
2017-06-11 15:48:53 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = function () {
+    return {
+        context: path.resolve(__dirname, 'app'),
+        entry: {
+            main: './index.js' 
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader?cacheDirectory=true',
+                        options: {
+                            presets: ['env'],
+                            plugins: [
+                                require('babel-plugin-transform-async-to-generator'),
+                                require('babel-plugin-transform-object-rest-spread')
+                            ]
+                        },
+                    }
+                }
+            ]
+        }
+    }
+}
